5cf73ca8158cb3a41643df9eab4ad5c322e71ebb
lseluga <loriann.higashi@yale.edu>
2025-01-25 14:04:12 -0500

    added CoralSubsystem

---
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
+import frc.robot.Constants.CanIdConstants;
+import frc.robot.Constants.CoralManipulatorConstants;
+import frc.robot.Constants.MotorDefaultsConstants;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+
+public class CoralSubsystem extends SubsystemBase {
+ 
+    // import motor id
+    private final SparkMax m_coralMotor = new SparkMax(CanIdConstants.kCoralCanId, MotorDefaultsConstants.Neo550MotorType);
+ 
+    private double kSpeed = CoralManipulatorConstants.speed;
+
+    public CoralSubsystem () {
+    
+        // Apply the respective configurations to the SPARKS. Reset parameters before
+        // applying the configuration to bring the SPARK to a known good state. Persist
+        // the settings to the SPARK to avoid losing them on a power cycle.
+        m_coralMotor.configure(Configs.CoralManipulator.coralConfig, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+
+  }
+
+  public void pickup() {
+    m_coralMotor.set(kSpeed);
+  }
+
+  public void stop() {
+    m_coralMotor.set(0);
+  }
+
+  public void eject() {
+    m_coralMotor.set(-kSpeed);
+  }
+
+}
