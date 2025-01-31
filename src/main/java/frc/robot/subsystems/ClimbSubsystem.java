e33b4a620712774e4cd66ba415ce621bddb495c4
lseluga <loriann.higashi@yale.edu>
2025-01-31 17:15:18 -0500

    Created subsystem

---
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
+import frc.robot.Constants.CanIdConstants;
+import frc.robot.Constants.ClimberConstants;
+import frc.robot.Constants.MotorDefaultsConstants;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class ClimbSubsystem {
+    
+}
