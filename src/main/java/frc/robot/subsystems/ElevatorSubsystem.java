39408df6e07311534fa13874f34e79bd8aea84d3
lseluga <loriann.higashi@yale.edu>
2025-02-07 19:11:41 -0500

    made elevator PID THEORETICALY work
    position not velocity based, may need changing in the not present (idk how to spell future)

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,40 +1,40 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Configs;
-import frc.robot.Constants.CanIdConstants;
-import frc.robot.Constants.ClimberConstants;
-import frc.robot.Constants.ElevatorConstants;
-import frc.robot.Constants.MotorDefaultsConstants;
-
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ElevatorConstants;
+
 public class ElevatorSubsystem extends SubsystemBase {
-    private final TalonFX m_elevatorMotor = new TalonFX(24, "rio"); //change deviceID and canbus
-    TalonFXConfiguration config = new TalonFXConfiguration();
-    
-    private double kSpeed = ElevatorConstants.kElevatorSpeed;
+  private final TalonFX m_elevatorMotor = new TalonFX(24, "rio"); // change deviceID and canbus
+  TalonFXConfiguration config = new TalonFXConfiguration();
 
-    public ElevatorSubsystem() {
-        //Add configurations to Configs.java
-        m_elevatorMotor.getConfigurator().apply(config);
-        config.Slot0.kP = ElevatorConstants.kElevatorP;
-        config.Slot0.kI = ElevatorConstants.kElevatorI;
-        config.Slot0.kD = ElevatorConstants.kElevatorD;
-    }
+  private double kSpeed = ElevatorConstants.kElevatorSpeed;
 
-    public void moveUp() {
-        m_elevatorMotor.set(kSpeed);
-      }
-    
-      public void stop() {
-        m_elevatorMotor.set(0);
-      }
-    
-      public void moveDown() {
-        m_elevatorMotor.set(-kSpeed);
-      }
+  public ElevatorSubsystem() {
+    // Add configurations to Configs.java
+    m_elevatorMotor.getConfigurator().apply(config);
+    config.Slot0.kP = ElevatorConstants.kElevatorP;
+    config.Slot0.kI = ElevatorConstants.kElevatorI;
+    config.Slot0.kD = ElevatorConstants.kElevatorD;
+
+  }
+
+  public void moveUp() {
+    // create a position closed-loop request, voltage output, slot 0 configs
+    final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+
+    // set position to 10 rotations
+    m_elevatorMotor.setControl(m_request.withPosition(10));
+  }
+
+  public void stop() {
+    m_elevatorMotor.set(0);
+  }
+
+  public void moveDown() {
+    m_elevatorMotor.set(-kSpeed);
+  }
 }
