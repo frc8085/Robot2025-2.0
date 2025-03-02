2f947fab098ea04fa16ac94bad2b9dde5d03bf62
lseluga <loriann.higashi@yale.edu>
2025-03-02 09:04:14 -0500

    adding gamestates

---
+++ src/main/java/frc/robot/GameStates.java
@@ -0,0 +1,28 @@
+
+package frc.robot;
+
+public enum GameStates {
+
+    // blue is the left side of the robot, yellow is right (intake) side
+
+    HOME,
+    TRANSITION,
+    CORAL_SOURCE,
+    CORAL_REEF_BLUE_1,
+    CORAL_REEF_BLUE_2,
+    CORAL_REEF_BLUE_3,
+    CORAL_REEF_BLUE_4,
+    CORAL_REEF_YELLOW_1,
+    CORAL_REEF_YELLOW_2,
+    CORAL_REEF_YELLOW_3,
+    CORAL_REEF_YELLOW_4,
+    ALGAE_GROUND_BLUE,
+    ALGAE_REEF_BLUE_2,
+    ALGAE_REEF_BLUE_3,
+    ALGAE_NET_BLUE,
+    ALGAE_GROUND_YELLOW,
+    ALGAE_REEF_YELLOW_2,
+    ALGAE_REEF_YELLOW_3,
+    ALGAE_NET_YELLOW,
+
+}
