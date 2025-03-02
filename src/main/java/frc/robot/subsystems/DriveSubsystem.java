bde15721ff1f76a9e7094ce12438d2300fa240cf
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2025-03-02 14:34:40 -0500

    Added autonomous code to drive
    Untested

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.auto.AutoBuilder;
 
 import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
@@ -62,29 +63,37 @@ public class DriveSubsystem extends SubsystemBase {
   // getModulePositions(),
   // new Pose2d());
 
-  // private void configurePathPlanner() {
+  private void configurePathPlanner() {
 
-  // // Configure PathPlanner AutoBuilder
-  // AutoBuilder.configureHolonomic(
-  // this::getPose, // Robot pose supplier
-  // this::resetOdometry, // Method to reset odometry (will be called if your auto
-  // has a starting pose)
-  // this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT
-  // RELATIVE
-  // this::driveRobotRelative, // Method that will drive the robot given ROBOT
-  // RELATIVE ChassisSpeeds
-  // new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should
-  // likely live in your
-  // // Constants class
-  // new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-  // new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
-  // 4.5, // Max module speed, in m/s
-  // 0.4, // Drive base radius in meters. Distance from robot center to furthest
-  // module.
-  // new ReplanningConfig() // Default path replanning config. See the API for the
-  // options here
-  // ),
-  // () -> {
+    // Configure PathPlanner AutoBuilder
+    AutoBuilder.configureHolonomic(
+        this::getPose, // Robot pose supplier
+        this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+        this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+        this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+        new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your
+                                         // Constants class
+            new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+            new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+            4.5, // Max module speed, in m/s
+            0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+            new ReplanningConfig() // Default path replanning config. See the API for the options here
+        ),
+        () -> {
+          // Boolean supplier that controls when the path will be mirrored for the red
+          // alliance
+          // This will flip the path being followed to the red side of the field.
+          // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+          var alliance = DriverStation.getAlliance();
+          if (alliance.isPresent()) {
+            return alliance.get() == DriverStation.Alliance.Red;
+          }
+          return false;
+        },
+        this // Reference to this subsystem to set requirements
+    );
+  }
   // // Boolean supplier that controls when the path will be mirrored for the red
   // // alliance
   // // This will flip the path being followed to the red side of the field.
