c9b5ff277f546cb41c8f1e4b9d6a9976d115ded0
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2025-03-05 16:05:42 -0500

    added speed parameter to command

---
+++ src/main/java/frc/robot/commands/movement/AutoDriveMeters.java
@@ -0,0 +1,78 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.movement;
+
+import frc.robot.Constants.CommandScoreConstants;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AutoDriveMeters extends Command {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+    private final DriveSubsystem m_drive;
+    private double m_forwardMeters = 0;
+    private double m_sidewaysMeters = 0;
+    private double m_speed = 0;
+    boolean forwardReached = false;
+    boolean sidewaysReached = false;
+
+    public AutoDriveMeters(DriveSubsystem drive, double forwardMeters, double sidewaysMeters, double speed) {
+        m_drive = drive;
+        m_speed = speed;
+        m_forwardMeters = forwardMeters;
+        m_sidewaysMeters = sidewaysMeters;
+        addRequirements(m_drive);
+    }
+
+    // Reset the odometry when the command is scheduled
+    // Then run the drive command to travel backwards
+    @Override
+    public void initialize() {
+        super.initialize();
+        m_drive.resetOdometry(new Pose2d());
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        super.execute();
+
+        m_drive.drive(
+                m_speed,
+                forwardReached ? 0 : Math.signum(m_forwardMeters),
+                sidewaysReached ? 0 : Math.signum(m_sidewaysMeters),
+                0,
+                false);
+    }
+
+    // Stop driving when the command ends or is interrupted
+    @Override
+    public void end(boolean interrupted) {
+        m_drive.stop();
+    }
+
+    // End the command when we reach the desired pose in meters
+    @Override
+    public boolean isFinished() {
+        double currentForwardPose = m_drive.getPose().getX();
+        double currentSidewaysPose = m_drive.getPose().getY();
+        // Stop when the current position reaches
+        // the desired backwards travel distance in meters
+
+        if (m_forwardMeters > 0) {
+            forwardReached = currentForwardPose >= m_forwardMeters;
+        } else {
+            forwardReached = currentForwardPose <= m_forwardMeters;
+        }
+
+        if (m_sidewaysMeters > 0) {
+            sidewaysReached = currentSidewaysPose >= m_sidewaysMeters;
+        } else {
+            sidewaysReached = currentSidewaysPose <= m_sidewaysMeters;
+        }
+
+        return forwardReached && sidewaysReached;
+    }
+}
