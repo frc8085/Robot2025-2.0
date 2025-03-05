56eb5b3abba6460eaac30eb82fecc48d7c901389
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2025-03-05 18:15:10 -0500

    liam math

---
+++ src/main/java/frc/robot/commands/AlignHeading.java
@@ -0,0 +1,64 @@
+package frc.robot.commands;
+
+import java.lang.annotation.Target;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.LimelightHelpers;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
+
+public class AlignHeading extends Command {
+    private final DriveSubsystem drive;
+    private final LimelightSubsystem limelight;
+
+    private final double xOffset;
+
+    private final double maxSpeed = 0.5;
+    private final double maxRotationSpeed = 0.4;
+
+    private final double base = 0; // tune
+    private final double thetaFinal = 0;
+
+    public boolean hasTarget = true;
+
+    public AlignHeading(DriveSubsystem drive, LimelightSubsystem limelight) {
+        this.drive = drive;
+        this.limelight = limelight;
+
+        xOffset = limelight.getX("limelight-left");
+
+        addRequirements(drive);
+    }
+
+    @Override
+    public void execute() {
+        if (!limelight.hasTarget("limelight-left")) {
+            hasTarget = false;
+            return;
+        }
+
+        // Retrieve AprilTag pose data
+
+        double xAbs = Math.abs(xOffset);
+        double baseAbs = xAbs * base;
+
+        double thetaInitial = Math.atan(xAbs / baseAbs);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drive.drive(0, 0, 0, 0, true);
+    }
+
+    @Override
+    public boolean isFinished() {
+        // probably a better way to do this
+        if (hasTarget) {
+            return true;
+        } else {
+            return true;
+        }
+    }
+}
