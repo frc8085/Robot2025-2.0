039e5cd7bce08bb7cbd01831318d4a8e09bf1d18
lseluga <loriann.higashi@yale.edu>
2025-03-07 00:23:25 -0500

    clean up unneeded code

--- src/main/java/frc/robot/commands/scoring/ScoreCoralL1.java
+++ src/main/java/frc/robot/commands/scoring/ScoreCoralL1.java
@@ -1,11 +1,8 @@
 package frc.robot.commands.scoring;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.commands.EjectCoral;
 import frc.robot.commands.states.ToCoralDropOff1;
-import frc.robot.commands.states.ToHomeCommand;
 import frc.robot.subsystems.CoralSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.PivotSubsystem;
@@ -18,11 +15,6 @@ public class ScoreCoralL1 extends SequentialCommandGroup {
                                 new ToCoralDropOff1(elevatorSubsystem, pivotSubsystem, yellow),
                                 new WaitUntilCommand(elevatorSubsystem::elevatorAtCoralDropOff1Height),
                                 new WaitUntilCommand(() -> pivotSubsystem.pivotAtCoralDropOffAngle(yellow)));
-                // new WaitCommand(.5),
-                // new EjectCoral(coralSubsystem, elevatorSubsystem, pivotSubsystem),
-                // new WaitCommand(0.25),
-                // new ToHomeCommand(elevatorSubsystem, pivotSubsystem,
-                // coralSubsystem));
         }
 
 }
