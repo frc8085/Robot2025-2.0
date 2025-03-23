b431a6bb7c1c49b2124a6f3e801e318ab276ccc8
lseluga <loriann.higashi@yale.edu>
2025-03-23 16:03:06 -0400

    updated the Oppo - L4 auto

--- src/main/java/frc/robot/commands/autoCommands/AutoOppoToL4.java
+++ src/main/java/frc/robot/commands/autoCommands/AutoOppoToL4.java
@@ -4,26 +4,37 @@ import choreo.Choreo;
 import choreo.trajectory.SwerveSample;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.drivetrain.SwerveDriveChoreoFollow;
+import frc.robot.subsystems.AlgaeSubsystem;
+import frc.robot.subsystems.CoralSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.PivotSubsystem;
 
 public class AutoOppoToL4 extends SequentialCommandGroup {
-    public AutoOppoToL4(DriveSubsystem driveSubsystem) {
+    public AutoOppoToL4(DriveSubsystem driveSubsystem, AlgaeSubsystem algaeSubsystem,
+            ElevatorSubsystem elevatorSubsystem, PivotSubsystem pivotSubsystem, CoralSubsystem coralSubsystem) {
 
         addCommands(
                 new SwerveDriveChoreoFollow(
                         driveSubsystem,
                         Choreo.<SwerveSample>loadTrajectory("OppoBargeToReef22"), true),
+                new AutoScoreCoralL4(algaeSubsystem, elevatorSubsystem, pivotSubsystem, coralSubsystem, driveSubsystem,
+                        isScheduled()),
                 new SwerveDriveChoreoFollow(
                         driveSubsystem,
                         Choreo.<SwerveSample>loadTrajectory("Reef22ToSource"), false),
                 new SwerveDriveChoreoFollow(
                         driveSubsystem,
                         Choreo.<SwerveSample>loadTrajectory("SourceToReef17L"), false),
+                new AutoScoreCoralL4(algaeSubsystem, elevatorSubsystem, pivotSubsystem, coralSubsystem, driveSubsystem,
+                        isScheduled()),
                 new SwerveDriveChoreoFollow(
                         driveSubsystem,
                         Choreo.<SwerveSample>loadTrajectory("Reef17LToSource"), false),
                 new SwerveDriveChoreoFollow(
                         driveSubsystem,
-                        Choreo.<SwerveSample>loadTrajectory("SourceToReef17R"), false));
+                        Choreo.<SwerveSample>loadTrajectory("SourceToReef17R"), false),
+                new AutoScoreCoralL4(algaeSubsystem, elevatorSubsystem, pivotSubsystem, coralSubsystem, driveSubsystem,
+                        isScheduled()));
     }
 }
