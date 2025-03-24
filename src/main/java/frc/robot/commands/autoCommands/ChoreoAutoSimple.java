08a15591096611b4932b6d09f60d97a02049b8b6
lseluga <loriann.higashi@yale.edu>
2025-03-23 21:28:28 -0400

    testing

--- src/main/java/frc/robot/commands/autoCommands/ChoreoAutoSimple.java
+++ src/main/java/frc/robot/commands/autoCommands/ChoreoAutoSimple.java
@@ -1,18 +1,7 @@
 package frc.robot.commands.autoCommands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.commands.manipulator.coral.EjectCoral;
-import frc.robot.commands.states.ToCoralDropOff4;
-import frc.robot.subsystems.AlgaeSubsystem;
-import frc.robot.subsystems.CoralSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.PivotSubsystem;
 import frc.robot.commands.drivetrain.SwerveDriveChoreoFollow;
 import choreo.Choreo;
 import choreo.trajectory.SwerveSample;
