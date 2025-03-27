8468570bd0cb4ff021e75955df1f5e3c98713cc7
lseluga <loriann.higashi@yale.edu>
2025-03-26 21:10:52 -0400

    Create ChoreoAutoCenterBarge.java

---
+++ src/main/java/frc/robot/commands/autoCommands/ChoreoAutoCenterBarge.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.autoCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.commands.drivetrain.SwerveDriveChoreoFollow;
+import choreo.Choreo;
+import choreo.trajectory.SwerveSample;
+import choreo.trajectory.Trajectory;
+
+import java.util.Optional;
+
+public class ChoreoAutoCenterBarge extends SequentialCommandGroup {
+    public ChoreoAutoCenterBarge(DriveSubsystem driveSubsystem) {
+
+        Optional<Trajectory<SwerveSample>> trajectory = Choreo.loadTrajectory("CenterBargeToReef21");
+
+        addCommands(
+                new SwerveDriveChoreoFollow(
+                        driveSubsystem,
+                        trajectory, true));
+                        
+    }
+}
