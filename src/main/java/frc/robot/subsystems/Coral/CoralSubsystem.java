6929040aae609f1aada7e8b9c6abee7b6b1db926
lseluga <loriann.higashi@yale.edu>
2025-10-12 18:14:52 -0400

    added new auto for L1 sequence
    note that ChoreoOppo2 has errors and needs to be fixed or deleted before deploy

--- src/main/java/frc/robot/subsystems/Coral/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/Coral/CoralSubsystem.java
@@ -14,7 +14,6 @@ import frc.robot.Constants.MotorDefaultsConstants;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkMax;
 
 public class CoralSubsystem extends SubsystemBase {
 
